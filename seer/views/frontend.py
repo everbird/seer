c6acda879450cfdaf864ca3cdffb29a364c30c81
Stephen Zhuang <stephen.zhuang@gmail.com>
2012-11-11 13:47:04 +0800

    Add privacy prolicy page for win8 app

--- seer/views/frontend.py
+++ seer/views/frontend.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Module
+from flask import Module, render_template
 
 
 frontend = Module(__name__)
@@ -9,3 +9,7 @@ frontend = Module(__name__)
 @frontend.route('/')
 def index():
     return 'ok'
+
+@frontend.route('/win8/privacy_policy')
+def win8_privacy_plicy():
+    return render_template('privacy_prolicy.jade')
