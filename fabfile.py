cbc94397a89bda92ddf4157eafcf8c5cfe497665
Stephen Zhuang <stephen.zhuang@gmail.com>
2012-11-24 20:26:20 +0800

    Make fabric play good with virtualenvwrapper

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import hosts, run, cd
+from fabric.api import hosts, run, cd, prefix
 from fabric.utils import puts
 
 @hosts('rainbow')
@@ -14,12 +14,17 @@ def update():
         run('/home/everbird/.virtualenvs/seer/bin/supervisorctl restart web')
     puts('Updated')
 
+def vrun(cmd, env='seer'):
+    remote_cmd = "/bin/bash -l -c 'source /usr/bin/virtualenvwrapper.sh" \
+        + " && workon " + env \
+        + " && " + cmd + "'"
+    run(remote_cmd)
+
 @hosts('rainbow')
 def rebuild():
     puts('Rebuild database...')
-    run('cd /home/everbird/product/seer'
-            ' && make drop_db_immeditely'
-            ' && make create_db'
-            ' && make init_db'
-            ' && make fetch_all'
-            ' && make online')
+    vrun("make drop_db_immeditely")
+    vrun("make create_db")
+    vrun('make init_db')
+    vrun('make fetch_all')
+    vrun('make online')
