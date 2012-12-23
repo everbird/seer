4ede28f5fb3889fb8dafc401daf9eec0f6892feb
Stephen Zhuang <stephen.zhuang@gmail.com>
2012-12-23 21:21:14 +0800

    Add a simple /hot page to show good movies on TV

--- seer/views/frontend.py
+++ seer/views/frontend.py
@@ -3,6 +3,7 @@
 
 from flask import Module, render_template
 
+from seer.models.program import Program
 
 frontend = Module(__name__)
 
@@ -13,3 +14,9 @@ def index():
 @frontend.route('/win8/privacy_policy')
 def win8_privacy_plicy():
     return render_template('privacy_prolicy.jade')
+
+@frontend.route('/hot')
+def hot_programs():
+    top250_movies = Program.query.get_top_programs()
+    mapped_movies = Program.query.get_mapped_programs()
+    return render_template('hot.jade', **locals())
