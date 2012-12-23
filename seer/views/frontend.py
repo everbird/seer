fc9c38f35640bd6e34dc38f7f68c07dc1f543701
Stephen Zhuang <stephen.zhuang@gmail.com>
2012-12-23 22:55:16 +0800

    Add /douban/all page to show all mapped programes

--- seer/views/frontend.py
+++ seer/views/frontend.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Module, render_template
+from flask import Module, render_template as rt
 
 from seer.models.program import Program
 
@@ -16,7 +16,14 @@ def win8_privacy_plicy():
     return render_template('privacy_prolicy.jade')
 
 @frontend.route('/hot')
+@frontend.route('/douban/hot')
 def hot_programs():
     top250_movies = Program.query.get_top_programs()
     mapped_movies = Program.query.get_mapped_programs()
-    return render_template('hot.jade', **locals())
+    return rt('hot.jade', **locals())
+
+@frontend.route('/douban/all')
+def douban_programs():
+    mapped_movies = Program.query.get_mapped_programs(rating_threshold=0,
+            rate_num_threshold=0)
+    return rt('all.jade', **locals())
