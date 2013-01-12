14f77bfd2ee07831278d8272265dca855b0d0148
Stephen Zhuang <stephen.zhuang@gmail.com>
2013-01-12 11:05:22 +0800

    Package synchronically.

--- seer/tools/package.py
+++ seer/tools/package.py
@@ -13,6 +13,7 @@ cmd_template = string.Template('wget -O - -c "$url" | gzip > "$target"')
 CMD_WGET_WITH_GZIP = 'wget -O - -c "%s" | gzip > "%s"'
 
 def package(target, datenum=None):
+    target = target or '.'
     datenum = datenum or datetime.now().strftime('%Y%m%d')
 
     query = dict(
@@ -32,5 +33,5 @@ def package(target, datenum=None):
     fname = 'daily-programs-%s.json.gz' % datenum
     output_path = os.path.join(target, fname)
     cmd = CMD_WGET_WITH_GZIP % (source, output_path)
-    r = os.popen(cmd)
-    print r
+    os.system(cmd)
+    print 'done.', output_path
