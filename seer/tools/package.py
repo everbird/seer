7bdde2946529aa7445aec1733712a854a13b261f
Stephen Zhuang <stephen.zhuang@gmail.com>
2013-01-13 20:37:57 +0800

    Skip packaging if remote not ok.

--- seer/tools/package.py
+++ seer/tools/package.py
@@ -3,6 +3,7 @@
 import os
 import string
 import json
+import requests
 from datetime import datetime
 from urllib import urlencode
 
@@ -33,8 +34,16 @@ def package(target=None, datenum=None, days=1):
         )
         source = config.SHOWN_HOME + '/api/program?' + urlencode(payload)
 
+        if not is_remote_ok(source):
+            print 'Data not repared for', _datenum
+            continue
+
         fname = 'daily-programs-%s.json.gz' % _datenum
         output_path = os.path.join(target, fname)
         cmd = CMD_WGET_WITH_GZIP % (source, output_path)
         os.system(cmd)
         print '[%s] done.'%i, output_path
+
+def is_remote_ok(url):
+    r = requests.get(url)
+    return bool(r.json['objects'])
