3c3413a562106fb2b03e000ae050a5e6ebf66dc8
Stephen Zhuang <stephen.zhuang@gmail.com>
2013-01-19 02:25:54 +0800

    Add Flask-Cache as extension

--- seer/extensions.py
+++ seer/extensions.py
@@ -5,10 +5,12 @@ from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
 from flask_redis import Redis
+from flask_cache import Cache
 
-__all__ = ['db', 'admin', 'manager', 'redis']
+__all__ = ['db', 'admin', 'manager', 'redis', 'cache']
 
 db = SQLAlchemy()
 admin = Admin()
 manager = APIManager()
 redis = Redis()
+cache = Cache()
