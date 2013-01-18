8cd3fdde737cfc0108be487693005aa013c2bdb0
Stephen Zhuang <stephen.zhuang@gmail.com>
2013-01-19 00:40:10 +0800

    Add Flask-And-Redis as extension

--- seer/extensions.py
+++ seer/extensions.py
@@ -4,9 +4,11 @@
 from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
+from flask_redis import Redis
 
-__all__ = ['db', 'admin', 'manager']
+__all__ = ['db', 'admin', 'manager', 'redis']
 
 db = SQLAlchemy()
 admin = Admin()
 manager = APIManager()
+redis = Redis()
